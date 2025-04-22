544c3cacacf0b156209d1999a1d33fd4a682ad30
Diego Macrini <macrini@proximify.ca>
2025-04-22 16:29:29 -0400

    clean up

--- bin/cli.js
+++ bin/cli.js
@@ -1,19 +1,21 @@
 #!/usr/bin/env node
 import { Command } from "commander";
 import { getPackageData } from "./utils/package.js";
-
 import { ToolHandler } from "@uniwebcms/dev-tools";
 import chalk from "chalk";
 import { table } from "table";
 
 try {
   const packageData = await getPackageData();
-  packageData.name = "uniweb";
-  packageData.description = "Manager for sites and modules";
+  const info = {
+    name: "uniweb",
+    description: "Manager for sites and modules",
+    version: packageData.version,
+  };
 
   const program = new Command();
   const toolHandler = new ToolHandler();
-  toolHandler.registerCommands(program, packageData, { chalk, table });
+  toolHandler.registerCommands(program, info, { chalk, table });
   program.parse(); // Run the CLI
 } catch (error) {
   console.error("Fatal error:", error);
